6e3fd5d20d54032966f3b1bbe95cbc86ff1b0819
VinhChamChi <vinh14072006@gmail.com>
2026-06-29 13:56:31 +0700

    first update

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<?php
+echo '<h1 style="color:red;">Name:Trần Quang Vinh </h1>';
+echo '<h1 style="color:red;">MSSV: 0306241421 </h1>';
+echo '<h1 style="color:red;">Class: CĐ CNTT 24WEB D </h1>';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>MANAGE_STUDENT</title>
+    *{
+        margin:0;
+        padding:0;
+    }
+</head>
+<body>
+    
+</body>
+</html>
